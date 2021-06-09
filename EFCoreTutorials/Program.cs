40fa23c4ac23a4ca182e32f3bc5ebe4337bf3a1e
Wismy Seide <WismySeide@outlook.com>
2021-06-08 21:54:40 -0400

    Add project files.

---
+++ EFCoreTutorials/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EFCoreTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+public class Student
+{
+    public int StudentId { get; set; }
+    public string Name { get; set; }
+}
+
+public class Course
+{
+    public int CourseId { get; set; }
+    public string CourseName { get; set; }
+}
+
+namespace EFCoreTutorials
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+        }
+    }
+}
